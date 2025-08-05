f9c76dbe31d2e82e9b6ef08ab81e1adb4a669f3c
Mark Nahano <marknahano@gmail.com>
2025-08-05 18:19:56 +0300

    Focus mode base starters

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.routes import focus_mode
+
 
 app = FastAPI()
+app.include_router(focus_mode.router, prefix="/focus")
+
+
 
 @app.get('/')
 async def hello():
--- Backend/app/routes/focus_mode.py
+++ Backend/app/routes/focus_mode.py
@@ -2,9 +2,40 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+on_focus = False
+off_focus = True
+
+
+
 @router.post('/focus/start')
 async def start_focus():
+    global on_focus
+    if on_focus == False:
+        on_focus = True
+        print("Timer has been started")
+    else : 
+        print("Error")
     return {'message': 'Focus mode started'}
 
 #@router.get means get something or print maybe even show something then @router.post is like creating a new task or starting something new it can also be interpreted as making something new.
+
+"""POST means: "I want to create something" (like start a focus session).
+
+GET means: "I want to fetch something" (like check focus session status).
+
+DELETE means: "I want to remove something."
+
+PUT / PATCH means: "I want to update something."""
+
+@router.post('/focus/stop')
+async def stop_focus():
+    global off_focus
+    if off_focus == True:
+        off_focus = False
+        print("focus off")
+    else : 
+        print("Error")
+    return {"message": "Focus session stopped"}
+
+
 
